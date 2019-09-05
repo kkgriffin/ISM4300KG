eb9cc1bb273ed1955093f94356e1381a90af89b2
Kyle Griffin <Kkylegriffin@gmail.com>
2019-09-05 17:00:38 -0400

    Class Project - Deliverable 1

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("");
+            Console.WriteLine("Please click any button to exit the application screen");
+        }
+    }
+}
