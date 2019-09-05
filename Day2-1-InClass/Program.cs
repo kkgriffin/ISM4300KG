ad38ab3981122fc02979fd366901adaeb7d725ea
Kyle Griffin <Kkylegriffin@gmail.com>
2019-09-05 16:09:48 -0400

    Day 2-1-InClass

---
+++ Day2-1-InClass/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Day2_1_InClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Please enter your age: ");
+                int age = int.Parse(Console.ReadLine());
+
+                if (age > 20)
+                {
+                    Console.WriteLine("You're old enought to drink");
+                }
+
+                else Console.WriteLine("You're not old enough to drink");
+             
+
+            }
+            catch
+            {
+                Console.WriteLine("You have an error!");
+            }
+        }
+    }
+}
