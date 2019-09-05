fec389ac30c7450fd71cdbed03796d7e8a0f6e4f
Kyle Griffin <Kkylegriffin@gmail.com>
2019-09-05 16:42:47 -0400

    DaysOfLeave in class assignment

---
+++ Day2-2-DaysOfLeave/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Day2_2_DaysOfLeave
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+
+                Console.WriteLine("How many days of leave would you like to take off work?");
+                int days = int.Parse(Console.ReadLine());
+
+                if (days > 1 && days < 7)
+                {
+                    Console.WriteLine("You are granted these days off");
+                }
+
+                if (days == 0)
+                {
+                    Console.WriteLine("Please enter a number greater than 0");
+                }
+
+                else Console.Write("You are not granted these days off");
+
+            }
+
+            catch
+            {
+                Console.WriteLine("Please adjust days input");
+            }
+            
+        }
+    }
+}
