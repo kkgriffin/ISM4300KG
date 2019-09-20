fea4d9a8b084232e2c73a70c2d539e43a09e952f
Kyle Griffin <Kkylegriffin@gmail.com>
2019-09-19 20:48:31 -0400

    Class Project - Deliverable 3 - Methods

---
+++ Class Project - Deliverable 3 - Methods/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Class_Project___Deliverable_3___Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Please input your name");
+                string Name = HelloName();
+
+                Console.WriteLine("Hello " + Name + "!");
+
+            }
+
+            catch
+            {
+                Console.WriteLine("An error was present in the code. Please try again. ");
+            }
+        }
+
+        private static string HelloName()
+        {
+            string UserName = Console.ReadLine();
+
+            return UserName;
+        }
+    
+    }
+}
