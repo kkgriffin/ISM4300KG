daac9c562704e915dd31d82506858d47405dfa10
Kyle Griffin <Kkylegriffin@gmail.com>
2019-09-19 20:40:43 -0400

    Class Project - Deliverable 3 - Arrays

---
+++ Class Project - Deliverable 3 - Arrays/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Class_Project___Deliverable_3___Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                
+                int[] NumericalArray = new int[25];
+
+               
+                for (int i = 0; i < 25; i++)
+                {
+                   
+                    NumericalArray[i] = i;
+                    Console.WriteLine("Element value = " + (i + 1));
+
+                }
+            }
+
+            catch
+            {
+                Console.WriteLine("An error occured in the program. Please try to run it again.");
+            }
+        }
+    }
+}
